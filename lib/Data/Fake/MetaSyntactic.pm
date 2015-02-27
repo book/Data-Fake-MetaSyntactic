e56d3fbcfb9671e5110cd928f84e4e53c5feee8a
Philippe Bruhat (BooK) <book@cpan.org>
2015-02-28 00:28:32 +0100

    first implementation of Data::Fake::MetaSyntactic

---
+++ lib/Data/Fake/MetaSyntactic.pm
@@ -0,0 +1,31 @@
+package Data::Fake::MetaSyntactic;
+
+use strict;
+use warnings;
+use Exporter 5.57 qw( import );
+
+our @EXPORT = qw( fake_meta fake_metatheme );
+
+use Acme::MetaSyntactic;
+my @themes = grep $_ ne 'any', Acme::MetaSyntactic->themes;
+
+sub fake_meta {
+    my ( $theme, $count ) = @_;
+    $theme ||= 'any';
+    $count ||= 1;
+
+    my $meta = Acme::MetaSyntactic->new;
+    return sub {
+        $meta->name(
+            ref $theme eq 'CODE' ? $theme->() : $theme,
+            ref $count eq 'CODE' ? $count->() : $count,
+        );
+    };
+}
+
+sub fake_metatheme {
+    return sub { $themes[ rand @themes ] };
+}
+
+1;
+
