dde35fcddbc77c4bac237215613516d97e63d8f0
Philippe Bruhat (BooK) <book@cpan.org>
2015-02-28 00:51:54 +0100

    pick an actual random theme instead of theme 'any'
    Acme::MetaSyntactic::any keeps track of every theme it loads,
    which could eat up a lot of memory. Simply using a random theme
    seems more consistent with the expected usage.

--- lib/Data/Fake/MetaSyntactic.pm
+++ lib/Data/Fake/MetaSyntactic.pm
@@ -11,7 +11,7 @@ my @themes = grep $_ ne 'any', Acme::MetaSyntactic->themes;
 
 sub fake_meta {
     my ( $theme, $count ) = @_;
-    $theme ||= 'any';
+    $theme ||= $themes[ rand @themes ];
     $count ||= 1;
 
     my $meta = Acme::MetaSyntactic->new;
