2f932f3e6fa1686fec846fc530e401af2248289f
Philippe Bruhat (BooK) <book@cpan.org>
2015-03-01 00:54:25 +0100

    documentation for Data::Fake::MetaSyntactic

--- lib/Data/Fake/MetaSyntactic.pm
+++ lib/Data/Fake/MetaSyntactic.pm
@@ -25,3 +25,55 @@ sub fake_metatheme {
 
 1;
 
+__END__
+
+=head1 NAME
+
+Data::Fake::MetaSyntactic - Fake metasyntactic data generators
+ 
+=head1 SYNOPSIS
+ 
+    use Data::Fake::MetaSyntactic;
+
+    fake_metatheme()->();   # foo, donmartin, weekdays, etc.
+    fake_meta()->();        # titi, GING_GOYNG, wednesday, etc.
+ 
+=head1 DESCRIPTION
+ 
+This module provides fake data generators for L<Acme::MetaSyntactic>.
+ 
+All functions are exported by default.
+ 
+=head1 FUNCTIONS
+ 
+=head2 fake_meta
+ 
+    $generator = fake_name( $theme );
+ 
+Returns a generator that provides a randomly selected item from the
+given L<Acme::MetaSyntactic> theme.
+
+If no C<$theme> is given, a random theme is picked, among the installed
+ones.
+ 
+=head2 fake_metatheme
+ 
+    $generator = fake_metatheme();
+ 
+Returns a generator that provides a random L<Acme::MetaSyntactic> theme name,
+among the installed ones.
+
+=head1 AUTHOR
+ 
+Philippe Bruhat (BooK), <book@cpan.org>.
+ 
+=head1 COPYRIGHT
+
+Copyright 2015 Philippe Bruhat (BooK), all rights reserved.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
