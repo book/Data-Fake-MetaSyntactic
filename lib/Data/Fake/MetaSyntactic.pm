e95c18e23dcf45841c50a1295681bda22314da2b
Philippe Bruhat (BooK) <book@cpan.org>
2015-03-01 00:48:46 +0100

    always return a single item

--- lib/Data/Fake/MetaSyntactic.pm
+++ lib/Data/Fake/MetaSyntactic.pm
@@ -10,16 +10,12 @@ use Acme::MetaSyntactic ();
 my @themes = grep $_ ne 'any', Acme::MetaSyntactic->themes;
 
 sub fake_meta {
-    my ( $theme, $count ) = @_;
+    my ($theme) = @_;
     $theme ||= $themes[ rand @themes ];
-    $count ||= 1;
 
     my $meta = Acme::MetaSyntactic->new;
     return sub {
-        $meta->name(
-            ref $theme eq 'CODE' ? $theme->() : $theme,
-            ref $count eq 'CODE' ? $count->() : $count,
-        );
+        $meta->name( ref $theme eq 'CODE' ? $theme->() : $theme );
     };
 }
 
